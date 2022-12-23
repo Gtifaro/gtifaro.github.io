f65d5e3d8ae5196703052ad93153d41e1c27072a
Gabriel Rodríguez <gabrieltifaro2021@gmail.com>
2022-12-22 22:10:27 -0500

    Add a bit of content and select some colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
-
+import React from "react";
+import "./utils/styles.css";
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <React.Fragment>
+      <div className="App">
+        <h1 className="loading">Work in progress...</h1>
+        <div className="example1"></div>
+        <div className="example2"></div>
+        <div className="example3"></div>
+        <div className="example4"></div>
+      </div>
+    </React.Fragment>
   );
 }
 
