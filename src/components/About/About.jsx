eb51600ef314e562f774f97ab5e1f86eb58f2528
Gabriel Rodríguez <gabrieltifaro2021@gmail.com>
2023-01-01 19:53:50 -0500

    Add about tap, TODO imgs

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
+import {NavLink} from "react-router-dom";
 
 const AboutContainer = () => {
     return (
         <div className='About'>
-            About - Work in progress...
+            <div className='Content'>
+                <h1>I'm Gabriel, nice to meet you</h1>
+                <p className='BigDesc'>I am a Front-end developer located in Colombia, I started programming 4 years ago in flowcharts with <a href="https://raptor.martincarlisle.com">Raptor</a> and Java with Eclipse IDE. After, I study at <a href="http://www.senasofiaplus.edu.co">SENA</a> an ADIS (Analysis and Development of Information Systems) technologist. There, I learned about JavaScript ES6, HTML 5, CSS 6 and a bit of SQL, PHP and Mobile (Android) development.<br/><br/>
+                In the last year, I started getting work experience, I started studying at <a href="https://platzi.com">Platzi</a> together with my study group and now I'm learning about Node.JS, React.js, ML with Python and game development with Unity.<br/><br/>
+                I hope we can do amazing and innovative projects in the future.</p>
+                {/* <p className='BigDesc'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate harum porro error quod nulla soluta aliquid officiis numquam excepturi nisi. Nostrum labore delectus vitae. Voluptatibus repellat architecto quo doloremque iure. Lorem ipsum dolor, sit amet consectetur.<br/><br/>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate harum porro error quod nulla soluta aliquid officiis numquam excepturi nisi. Nostrum labore delectus vitae.<br/><br/>
+                Necessitatibus error voluptatum illum modi! Suscipit maxime porro repellendus aliquam nam perspiciatis provident.</p> */}
+                <NavLink className="WhoIsMe" to="/Skills">My work tools</NavLink>
+                <NavLink className="StartWorking" to="/Contact">¡Start creating!</NavLink>
+            </div>
+            <div className='Img'>
+
+            </div>
         </div>
     );
 };
