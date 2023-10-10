fc986ded6a15d6e1e24b3a555bf1d23223a584be
Gabriel Rodriguez <gabrieltifaro2021@gmail.com>
2023-10-10 13:57:01 -0500

    Improve navbar and prepare icon position

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,11 +1,73 @@
-import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar({ children }) {
   return (
     <>
-      <div className="bg-slate-50 w-full h-full">
-        <div className="bg-black border-l-red-600 w-[2.5%] h-screen fixed"></div>
-        <div>{children}</div>
+      <div>
+        <div className="bg-black w-[3%] h-full fixed shadow-navbar place-items-center grid">
+          <div className="w-full absolute top-0">
+            <Link href="/">
+              <Image
+                alt="Gabrial Antonio Rodríguez Tífaro - Gtifaro logo"
+                src="/G.svg"
+                className="w-full select-none cursor-pointer"
+                width={"1"}
+                height={"1"}
+              />
+            </Link>
+          </div>
+          <div className="h-fit  ">
+            <Link href="/">
+              <Image
+                alt="Gabrial Antonio Rodríguez Tífaro - Gtifaro logo"
+                src="/G.svg"
+                className="w-full select-none cursor-pointer "
+                width={"1"}
+                height={"1"}
+              />
+            </Link>
+            <Link href="/">
+              <Image
+                alt="Gabrial Antonio Rodríguez Tífaro - Gtifaro logo"
+                src="/G.svg"
+                className="w-full select-none cursor-pointer "
+                width={"1"}
+                height={"1"}
+              />
+            </Link>
+            <Link href="/">
+              <Image
+                alt="Gabrial Antonio Rodríguez Tífaro - Gtifaro logo"
+                src="/G.svg"
+                className="w-full select-none cursor-pointer "
+                width={"1"}
+                height={"1"}
+              />
+            </Link>
+            <Link href="/">
+              <Image
+                alt="Gabrial Antonio Rodríguez Tífaro - Gtifaro logo"
+                src="/G.svg"
+                className="w-full select-none cursor-pointer "
+                width={"1"}
+                height={"1"}
+              />
+            </Link>
+          </div>
+          <div className="w-full absolute bottom-0">
+            <Link href="/">
+              <Image
+                alt="Gabrial Antonio Rodríguez Tífaro - Gtifaro logo"
+                src="/G.svg"
+                className="w-full select-none cursor-pointer"
+                width={"1"}
+                height={"1"}
+              />
+            </Link>
+          </div>
+        </div>
+        <div className="w-full h-full bg-black">{children}</div>
       </div>
     </>
   );
