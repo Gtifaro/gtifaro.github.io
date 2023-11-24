a8a8cf7ce944f3287751a3766e19abe3784710c1
Gabriel Rodriguez <gabrieltifaro2021@gmail.com>
2023-11-24 07:42:44 -0500

    Improve navbar desing

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -31,18 +31,18 @@ export default function NavBar() {
     pathname = "/contact";
   }
 
-  const [hoveredPath, setHoveredPath] = useState(pathname);
+  const [hoveredPath, setHoveredPath] = useState(pathname || "/");
 
   return (
-    <div className="p-[0.4rem] rounded-lg mb-12 sticky top-4 z-[100]  backdrop-blur-md">
+    <div className="p-[0.4rem] w-full rounded-lg mb-12 fixed top-4 z-[100] backdrop-blur-md">
       <nav className="flex gap-2 relative justify-center w-full z-[100]">
-        {navItems.map((item, index) => {
+        {navItems.map((item) => {
           const isActive = item.path === hoveredPath;
           
           return (
             <Link
               key={item.path}
-              className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in ${
+              className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in float-left ${
                 isActive ? "text-zinc-100" : "text-zinc-400"
               }`}
               data-active={isActive}
