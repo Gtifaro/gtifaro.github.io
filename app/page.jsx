0ea1930345d0bcb379a695bad2cb478f940ad0d3
Gabriel Rodriguez <gabrieltifaro2021@gmail.com>
2023-11-25 08:50:15 -0500

    add scrool functionality to navbar and let improve at selected nav tag

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,19 @@
 export default function Home() {
+  //141414
   return (
     <main className="w-full">
-      <div className="bg-[#141414] h-[150vh]">
-        Nombre del man
+      <div className="bg-[#a33535] pt-[10vh] h-[150vh]">
+        <div className="bg-yellow-500">kjsnfkjdsnfjksdfkjsdnfjkdsnfsk</div>
       </div>
       <div id="about" className="bg-[#a33535] h-[150vh]">
-        Nombre del man
+        <div className="pt-[10vh]">kjsnfkjdsnfjksdfkjsdnfjkdsnfsk</div>
       </div>
       <div id="work" className="bg-[#5b46c5] h-[150vh]">
-        Nombre del man
+        <div className="pt-[10vh]">kjsnfkjdsnfjksdfkjsdnfjkdsnfsk</div>
       </div>
       <div id="contact" className="bg-[#46d277] h-[150vh]">
-        Nombre del man
+        <div className="pt-[10vh]">kjsnfkjdsnfjksdfkjsdnfjkdsnfsk</div>
       </div>
     </main>
-  )
+  );
 }
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { motion } from "framer-motion";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { usePathname } from "next/navigation";
 import Link from "next/link";
 
@@ -27,23 +27,43 @@ const navItems = [
 export default function NavBar() {
   let pathname = usePathname() || "/";
 
-  if (pathname.includes("/contact/")) {
-    pathname = "/contact";
+  if (pathname.includes("/#contact")) {
+    pathname = "/#contact";
+  }
+  if (pathname.includes("/#work")) {
+    pathname = "/#work";
+  }
+  if (pathname.includes("/#about")) {
+    pathname = "/#about";
+  }
+  if (pathname.includes("/")) {
+    pathname = "/";
   }
 
-  const [hoveredPath, setHoveredPath] = useState(pathname || "/");
+ 
+  const [hoveredPath, setHoveredPath] = useState(pathname);
+  const [scrollY, setScrollY] = useState(0);
+  
+  useEffect(() => {
+    function handleScroll() {
+      setScrollY(window.scrollY > 10)
+    }
+    window.addEventListener("scroll", handleScroll); 
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [scrollY])
 
   return (
-    <div className="p-[0.4rem] w-full rounded-lg mb-12 fixed top-4 z-[100] backdrop-blur-md">
-      <nav className="flex gap-2 relative justify-center w-full z-[100]">
+    <div className="p-[0.4rem] w-full rounded-lg mb-12 fixed top-2 z-[100] ">
+      <nav className={`flex gap-2 relative justify-center w-fit mx-auto rounded-full px-2 py-1 z-[100] ${
+                scrollY ? "backdrop-blur-sm" : ""
+              }`}>
         {navItems.map((item) => {
           const isActive = item.path === hoveredPath;
-          
           return (
             <Link
               key={item.path}
-              className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-300 ease-in float-left ${
-                isActive ? "text-zinc-100" : "text-zinc-400"
+              className={`px-4 py-2 rounded-md text-sm lg:text-base relative no-underline duration-100 ease-in float-left ${
+                isActive ? scrollY ? "text-zinc-100": "text-zinc-200" : scrollY ? "text-zinc-300": "text-zinc-400"
               }`}
               data-active={isActive}
               href={item.path}
