97a2ab1119ba33d75bef9ce3d66decc7cbecc4ca
GTifaro <gabrieltifaro2021@gmail.com>
2023-12-11 23:50:49 -0500

    Added a little of text to home

--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,17 @@
 export default function Home() {
   return (
     <main className="w-full">
-      <div id="home" className="bg-[#141414] pt-[4.25vw] h-screen text-white">
+      <div id="home" className="bg-[#141414] h-screen text-white flex items-center">
+        <div className="w-[25vw] mx-auto h-[12vw] p-[1vw] rounded-[.25vw] text-[1vw] bg-[#202020]">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ab atque nihil vero eaque corporis. Numquam error nobis reprehenderit fugit quia consectetur expedita maiores. Provident quis harum velit amet iste nesciunt!
+          {/* Desarrollador Frontend con más de 1 año de experiencia creando soluciones innovadoras con tecnologías como HTML, CSS, JavaScript, entre otras. Nacido en Bogotá, Colombia, hace 21 años y apasionado por la creación del futuro de la tecnología. */}
+        </div>
       </div>
-      <div id="about" className="bg-[#ca5b5b] h-screen text-white">
+      <div id="about" className="bg-[#141414] h-screen text-white">
       </div>
-      <div id="work" className="bg-[#6d5151] h-screen text-white">
+      <div id="work" className="bg-[#141414] h-screen text-white">
       </div>
-      <div id="contact" className="bg-[#6b45a8] h-screen text-white">
+      <div id="contact" className="bg-[#141414] h-screen text-white">
       </div>
     </main>
   );
