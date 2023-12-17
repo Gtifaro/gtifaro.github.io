4acd5ce40d5ce29edaeda21634870a56f61b6960
GTifaro <gabrieltifaro2021@gmail.com>
2023-12-17 14:33:05 -0500

    Change navbar focus color and background opacity

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -65,7 +65,7 @@ export default function NavBar() {
               <span>{item.name}</span>
               {item.path === hoveredPath && (
                 <motion.div
-                  className="absolute bottom-0 left-0 h-full bg-[#202020] rounded-full -z-10"
+                  className="absolute bottom-0 left-0 h-full bg-[#333] rounded-full -z-10"
                   layoutId="navbar"
                   aria-hidden="true"
                   style={{
